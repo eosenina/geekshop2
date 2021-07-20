05334cb3558e0dd48e3b9e1d403db698d535d21b
Екатерина Осенина <ekaterinaosenina@macbook.osenin.me>
2021-07-20 12:25:03 +0300

    homework7

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,21 @@
-import json
+from django.conf import settings
+from django.core.cache import cache
 import os
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from mainapp.models import ProductCategory, Product
+
+
+def get_categories():
+    if settings.LOW_CACHE:
+        key = 'menu_categories'
+        menu_categories = cache.get(key)
+        if menu_categories is None:
+            menu_categories = ProductCategory.objects.all()
+            cache.set(key, menu_categories)
+        return menu_categories
+    else:
+        return ProductCategory.objects.all()
 
 
 def index(request):
@@ -21,6 +34,6 @@ def products(request, category_id=None, page=1):
         prod_paginator = paginator.page(paginator.num_pages)
     context = {'title': 'Geekshop - Каталог',
                'product_list': prod_paginator,
-               'categories': ProductCategory.objects.all()}
+               'categories': get_categories()}
     return render(request, 'mainapp/products.html', context)
 
